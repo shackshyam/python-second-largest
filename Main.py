bd040fdde9f9e07c4f082673d833db37bdb6c954
SHACK <157804789+shackshyam@users.noreply.github.com>
2024-02-02 12:52:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,4 +4,6 @@ for i in range(0,Count):
     User_Indexes = int(input("Enter the Number : "))
     Num.append(User_Indexes)
 Num.sort()
+Num = list(set(Num))
+print(Num)
 print(f"The Second Largest Number is :- {Num[1]}")
